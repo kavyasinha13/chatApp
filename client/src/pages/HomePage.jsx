7301c849e67cdd9b052581de040c4fb51a39e066
Kavya <kavya01092003@gmail.com>
2025-07-25 15:12:29 +0530

    project completed

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import ChatContainer from "../components/ChatContainer";
 import RightSidebar from "../components/RightSidebar";
+import { ChatContext } from "../../context/ChatContext";
 
 const HomePage = () => {
-  const [selectedUser, setSelectedUser] = useState(false);
+  const selectedUser = useContext(ChatContext);
 
   return (
     <div className="border w-full h-screen p-8 sm:px-[15%] sm:py=[5%]">
@@ -17,10 +18,7 @@ const HomePage = () => {
       >
         <Sidebar />
         <ChatContainer />
-        <RightSidebar
-          selectedUser={selectedUser}
-          setSelectedUser={setSelectedUser}
-        />
+        <RightSidebar />
       </div>
     </div>
   );
--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -80,8 +80,7 @@ export const markMessageAsSeen = async (req, res) => {
 //send message to selected user
 export const sendMessage = async (req, res) => {
   try {
-    const { text } = req.body;
-    const image = req.file?.path;
+    const { text, image } = req.body;
     const receiverId = req.params.id;
     const senderId = req.user._id;
 
